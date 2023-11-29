b285b999ed12e66d7292bce37dfd34b0bc856f4a
roman zarza <romanlzarza32@gmail.com>
2023-11-29 01:02:30 -0300

    array & objetos

--- js/index.js
+++ js/index.js
@@ -1,50 +1,79 @@
+    let memoria = 0;
+
+    let test1 = [374, 375];
+
+    let base = ["nombre", 0]
+
 function calcularPrecio()
 { 
-let precio = prompt("Ingrese el precio del juego", "");
+    base[0] = prompt("Ingrese el nombre del juego", "");
 
-let impuesto1 = precio*0.25;
+    base[1] = prompt("Ingrese el precio del juego en USD", "")
 
-let impuesto2 = precio*0.3;
+    let precio = base[1]*374;
 
-let impuesto3 = precio*0.45;
+    let impuesto1 = precio*1;
 
-let contImpuestos = parseFloat(impuesto1+impuesto2+impuesto3);
+    let impuesto2 = precio*0.25;
 
-let precioFinal = parseFloat (precio) + parseFloat(impuesto1+impuesto2+impuesto3);
+    let impuesto3 = precio*0.3;
 
-alert("Al precio inicial "+ "($" + precio + ") " + "se le agregan: $"+ contImpuestos + " en impuestos, dando un total de: $" + precioFinal);
+    let contImpuestos = parseFloat(impuesto1+impuesto2+impuesto3);
 
-memoria = precioFinal;
+    let precioFinal = parseFloat (precio) + parseFloat(impuesto1+impuesto2+impuesto3);
 
-return memoria;
+let producto = {
+    nombreJuego: base[0],
+    pesificado: precio,
+    impuestos: contImpuestos,
+    preciosCimpuestos: precioFinal,
+};
+
+    alert("El juego '" + producto['nombreJuego'] + "' cuyo precio en pesos es: $" + parseFloat(producto['pesificado'].toFixed(2)) + ", y se le añaden $" + parseFloat(producto['impuestos'].toFixed(2)) + " en impuestos, dando un total de: $" + parseFloat(producto['preciosCimpuestos'].toFixed(2)));
+
+    memoria = precioFinal;
+
+    return memoria;
 }
 
 function sumarJuego()
 {
-    let precio = prompt("Ingrese el precio del juego", "");
+    base[0] = prompt("Ingrese el nombre del juego", "");
 
-    let impuesto1 = precio*0.25;
+    base[1] = prompt("Ingrese el precio del juego en USD", "")
 
-    let impuesto2 = precio*0.3;
+    let precio = base[1]*374;
 
-    let impuesto3 = precio*0.45;
+    let impuesto1 = precio*1;
+
+    let impuesto2 = precio*0.25;
+
+    let impuesto3 = precio*0.3;
 
     let contImpuestos = parseFloat(impuesto1+impuesto2+impuesto3);
 
     let precioFinal = parseFloat (precio) + parseFloat(impuesto1+impuesto2+impuesto3); 
 
-    sumatoria = parseFloat(precioFinal+memoria);
+    let sumatoria = parseFloat(precioFinal+memoria);
 
-    alert("Al precio inicial "+ "($" + precio + ") " + "se le agregan: $"+ contImpuestos + " en impuestos, dando un total de: $" + precioFinal + " y se le suman $"+ memoria + " de el/los juegos anteriores, dando un total de: $"+ sumatoria);
+let producto = {
+    nombreJuego: base[0],
+    pesificado: precio,
+    impuestos: contImpuestos,
+    preciosCimpuestos: precioFinal,
+    precioSumado: sumatoria,
+};
+
+    alert("Al valor inicial, ($"+ parseFloat(memoria.toFixed(2))+") se le suma el juego '" + producto['nombreJuego'] + "' cuyo precio en pesos es: $" + parseFloat(producto['pesificado'].toFixed(2)) + ", y se le añaden $" + parseFloat(producto['impuestos'].toFixed(2))+ " en impuestos, dando un total de: $" + parseFloat(producto['preciosCimpuestos'].toFixed(2)) + ". El total en conjunto a el/los anteriores juegos es de: $" + parseFloat(producto['precioSumado'].toFixed(2)));
 
     memoria = sumatoria;
 
     return memoria;
 }
 
-calcularPrecio();
+    calcularPrecio();
 
-let inicio = prompt("Si desea finalizar = 0, consultar el valor de otro juego = 1, sumar otro juego al total ingrese = 2", "");
+    let inicio = prompt("Si desea finalizar = 0, consultar el valor de otro juego = 1, sumar otro juego al total ingrese = 2", "");
 
 while (inicio != 0)
 {
